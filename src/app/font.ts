4fb05d4d659aae8dec288e00e75e4c32e00f2bed
newswim <ofenixculpa@gmail.com>
2023-01-26 21:26:14 -0600

    add chart

---
+++ src/app/font.ts
@@ -0,0 +1,7 @@
+import { IBM_Plex_Mono } from '@next/font/google'
+
+export const plex = IBM_Plex_Mono({
+  subsets: ['latin'],
+  weight: ['700'],
+  variable: '--font-plex',
+})
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { plex } from './font'
 import './globals.css'
 
 export default function RootLayout({
@@ -6,7 +7,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={plex.className}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
